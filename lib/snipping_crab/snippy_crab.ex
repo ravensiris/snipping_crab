00b1f76cf5a8190539931894e25578b08791210f
Maksymilian Jodłowski <maksymilian.jodlowski@curiosum.com>
2022-07-20 10:16:39 +0200

    Add NIF header

--- lib/snipping_crab/snippy_crab.ex
+++ lib/snipping_crab/snippy_crab.ex
@@ -2,6 +2,12 @@ defmodule SnippingCrab.SnippyCrab do
   use Rustler, otp_app: :snipping_crab, crate: "snippy_crab"
 
   # When your NIF is loaded, it will override this function.
-  @spec add(integer(), integer()) :: integer()
-  def add(a, b), do: :erlang.nif_error(:nif_not_loaded)
+  @spec crop_and_grayscale(
+          binary(),
+          non_neg_integer(),
+          non_neg_integer(),
+          non_neg_integer(),
+          non_neg_integer()
+        ) :: binary()
+  def crop_and_grayscale(image, x, y, width, height), do: :erlang.nif_error(:nif_not_loaded)
 end
