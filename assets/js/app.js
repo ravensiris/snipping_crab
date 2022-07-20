e935f4557818a0a1f6a7d59afc7dcde96898174b
Maksymilian Jodłowski <maksymilian.jodlowski@curiosum.com>
2022-07-20 09:13:52 +0200

    Setup JS code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,5 @@
-// We import the CSS which is extracted to its own file by esbuild.
-// Remove this line if you add a your own CSS build pipeline (e.g postcss).
 import "../css/app.css"
-
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
@@ -29,17 +7,42 @@ import topbar from "../vendor/topbar"
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
-// Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
-// connect if there are any LiveViews on the page
 liveSocket.connect()
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+// Import Croppr.js
+import Croppr from "croppr";
+import "../node_modules/croppr/src/css/croppr.css";
+
+// Load up all the needed elems
+let formElem = document.getElementById("image");
+let imagePreviewElem = document.getElementById("imagePreview");
+let imageFileElem = document.getElementById("image_file");
+let imageXElem = document.getElementById("image_x");
+let imageYElem = document.getElementById("image_y");
+let imageWidthElem = document.getElementById("image_width");
+let imageHeightElem = document.getElementById("image_height");
+
+imageFileElem.addEventListener("change", () => {
+    // Assume single file selected only
+    const [file] = imageFileElem.files;
+
+    // Required for croppr to work
+    imagePreviewElem.src = URL.createObjectURL(file);
+
+    let croppr = new Croppr(imagePreviewElem);
+
+    // Collect crop params before submit
+    formElem.addEventListener("submit", () => {
+        const {x, y, width, height} = croppr.getValue();
+        imageXElem.value = x;
+        imageYElem.value = y;
+        imageWidthElem.value = width;
+        imageHeightElem.value = height;
+    });
+})
